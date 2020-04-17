013f29cb79c94d11a9db3b0b7e43dafee4d673af
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-04-17 14:30:16 +0300

    IDto eklendi , uye ve uyedetay complex type ' ı eklendi

---
+++ StudyCheckWeb.Entities/Dtos/UserDetail.cs
@@ -0,0 +1,27 @@
+﻿using StudyCheck.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyCheckWeb.Entities.Dtos
+{
+    /// <summary>
+    /// Uye ve Uyedetay tablosunun birleşimi
+    /// </summary>
+    public class UserDetail:IDto
+    {
+        public int uye_id { get; set; }
+        public int uyedetay_id { get; set; }
+        public string uye_ad { get; set; }
+        public string uye_soyad { get; set; }
+        public string kullanici_adi { get; set; }
+        public string kullanici_sifre { get; set; }
+        public string kullanici_mail { get; set; }
+        public DateTime kayit_tarihi { get; set; }
+        public DateTime guncelleme_tarihi{ get; set; }
+        public int sil_id { get; set; }
+        public int tema_id { get; set; }
+        public int rol_id { get; set; }
+        public int guncelleyen_id { get; set; }
+    }
+}
