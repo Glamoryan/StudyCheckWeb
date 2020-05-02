47442475a0a938a40bd8e9afa95d0399618ccc91
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-05-02 19:06:38 +0300

    Rol ve Yetki controller,viewleri eklendi ve düzenlendi

---
+++ StudyCheckWeb.MvcWebUI/Areas/Administrator/Controllers/RolController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudyCheckWeb.MvcWebUI.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class RolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RolListesi()
+        {
+            return View();
+        }
+
+        public IActionResult RolEkle()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudyCheckWeb.MvcWebUI/Areas/Administrator/Controllers/YetkiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudyCheckWeb.MvcWebUI.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class YetkiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult YetkiListesi()
+        {
+            return View();
+        }
+
+        public IActionResult YetkiEkle()
+        {
+            return View();
+        }
+    }
+}
