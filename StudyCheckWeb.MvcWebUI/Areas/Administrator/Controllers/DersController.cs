93a69a9bd1cd9bf89af7275f5bf0a27699d54d72
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-05-02 20:35:04 +0300

    admin paneldeki menüler aktifleştirildi

---
+++ StudyCheckWeb.MvcWebUI/Areas/Administrator/Controllers/DersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudyCheckWeb.MvcWebUI.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class DersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DersListesi()
+        {
+            return View();
+        }
+
+        public IActionResult DersEkle()
+        {
+            return View();
+        }
+    }
+}
