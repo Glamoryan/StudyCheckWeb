eeaf3b38ea8a4d3fb995f97c500d5cb18299aeef
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-06-30 20:25:43 +0300

    Sınav ekleme etkinleştirildi

--- StudyCheckWeb.MvcWebUI/Areas/Administrator/Controllers/SinavController.cs
+++ StudyCheckWeb.MvcWebUI/Areas/Administrator/Controllers/SinavController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 using StudyCheck.Utilities.CustomExceptions;
 using StudyCheckWeb.Business.Abstract;
+using StudyCheckWeb.Entities.Concrete;
 using StudyCheckWeb.MvcWebUI.Areas.Administrator.Models;
+using StudyCheckWeb.MvcWebUI.Authentication;
 
 namespace StudyCheckWeb.MvcWebUI.Areas.Administrator.Controllers
 {
@@ -18,11 +21,13 @@ namespace StudyCheckWeb.MvcWebUI.Areas.Administrator.Controllers
         IUyedetayService _uyedetayService;
         ISinavService _sinavService;
         EntityListModel _entityListModel;
+        UserManager<User> _userManager;
 
-        public SinavController(IUyedetayService uyedetayService, ISinavService sinavService)
+        public SinavController(IUyedetayService uyedetayService, ISinavService sinavService, UserManager<User> userManager)
         {
             _uyedetayService = uyedetayService;
             _sinavService = sinavService;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -46,7 +51,7 @@ namespace StudyCheckWeb.MvcWebUI.Areas.Administrator.Controllers
         }
 
         [HttpPost]
-        public IActionResult SinavEkle(string sinavAdi,int durum,string sinavTarihi)
+        public async Task<IActionResult> SinavEkle(string sinavAdi,int durum,string sinavTarihi)
         {
             try
             {
@@ -54,6 +59,26 @@ namespace StudyCheckWeb.MvcWebUI.Areas.Administrator.Controllers
                     throw new RequiredFieldsException("Sınav adı boş bırakılamaz");
                 if(sinavTarihi == null)
                     throw new RequiredFieldsException("Sınav tarihi boş bırakılamaz");
+
+                var sinav = _sinavService.GetAll().Where(s => s.sinav_ad.ToLower() == sinavAdi.ToLower()).FirstOrDefault();
+                if (sinav != null)
+                    throw new Exception("Bu sınav zaten kayıtlı");
+                else
+                {
+                    var identityUser = await _userManager.GetUserAsync(HttpContext.User);
+                    Sinav sinavimiz = new Sinav
+                    {
+                        sinav_ad = sinavAdi,
+                        sinav_tarih = Convert.ToDateTime(sinavTarihi),
+                        eklenme_tarihi = DateTime.Now,
+                        ekleyen_id = identityUser.uyeDetayId,
+                        guncelleme_tarihi = DateTime.Now,
+                        sil_id = durum,
+                        guncelleyen_id = identityUser.uyeDetayId
+                    };
+                    _sinavService.AddSinav(sinavimiz);
+                    ViewBag.Message = "Sınav başarıyla eklendi";
+                }
             }
             catch (Exception ex)
             {
