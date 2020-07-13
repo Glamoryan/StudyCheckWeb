4799629874346d9fcb02051fa69652ffcc8d877f
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-07-13 19:29:47 +0300

    StudyPanelde ders bilgileri ajax ile çekildi

--- StudyCheck.Utilities/CustomModels/CalismaBilgileri.cs
+++ StudyCheck.Utilities/CustomModels/CalismaBilgileri.cs
@@ -6,6 +6,7 @@ namespace StudyCheck.Utilities.CustomModels
 {
     public class CalismaBilgileri
     {
+        public int data { get; set; } //1-veri var , 0-veri yok
         public string toplamCalisilanZaman { get; set; }
         public string sonCalisilanSinav { get; set; }
         public string sonCalisilanDers { get; set; }
@@ -14,5 +15,6 @@ namespace StudyCheck.Utilities.CustomModels
         public string derseCalisilanZaman { get; set; }
         public string sinavTarihi { get; set; }
         public string derseSonCalisilanZaman { get; set; }
+        public string sinavaKalanGun { get; set; }
     }
 }
--- StudyCheckWeb.MvcWebUI/Areas/Study/Controllers/HomeController.cs
+++ StudyCheckWeb.MvcWebUI/Areas/Study/Controllers/HomeController.cs
@@ -37,77 +37,130 @@ namespace StudyCheckWeb.MvcWebUI.Areas.Study.Controllers
             _dersService = dersService;
         }
 
-        public CalismaBilgileri studyInfos(List<Calisma> calismalar)
+        public CalismaBilgileri studyInfos(List<Calisma> calismalar,byte? durum=null,int? sinav_id=null,int? ders_id=null)
         {
             CalismaBilgileri bilgiler = new CalismaBilgileri();
 
-            if(calismalar.Count > 0)
+            if(durum == null)
             {
-                //Toplam Çalışılan Zaman
-                TimeSpan toplamZaman = TimeSpan.Zero;
-                foreach (var calisma in calismalar)
+                if (calismalar.Count > 0)
                 {
-                    toplamZaman += calisma.calisilan_zaman;
-                }
-                bilgiler.toplamCalisilanZaman = toplamZaman.ToString();                
-
-                //Son Sınav
-                int? sonSinavId = calismalar.LastOrDefault().sinav_id;
-                if (sonSinavId == null)
-                    bilgiler.sonCalisilanSinav = "-";
-                else
-                {
-                    var sinav = _sinavService.GetById((int)sonSinavId);
-                    bilgiler.sonCalisilanSinav = sinav.sinav_ad;
-                    bilgiler.sinavTarihi = sinav.sinav_tarih.ToShortDateString();
-                    bilgiler.derseSonCalisilanZaman = calismalar.LastOrDefault().calisilan_zaman.ToString();
-
-                    //Sınava Çalışılan Toplam Zaman
-                    TimeSpan sinavToplam = TimeSpan.Zero;
+                    //Toplam Çalışılan Zaman
+                    TimeSpan toplamZaman = TimeSpan.Zero;
                     foreach (var calisma in calismalar)
                     {
-                        if(calisma.sinav_id == sinav.id)
-                            sinavToplam += calisma.calisilan_zaman;
+                        toplamZaman += calisma.calisilan_zaman;
                     }
-                    bilgiler.sinavaCalisilanZaman = sinavToplam.ToString();
-                }
+                    bilgiler.toplamCalisilanZaman = toplamZaman.ToString();
 
-                //Son Ders
-                int? sonDersId = calismalar.LastOrDefault().ders_id;
-                if (sonDersId == null)
+                    //Son Sınav
+                    int? sonSinavId = calismalar.LastOrDefault().sinav_id;
+                    if (sonSinavId == null)
+                        bilgiler.sonCalisilanSinav = "-";
+                    else
+                    {
+                        var sinav = _sinavService.GetById((int)sonSinavId);
+                        bilgiler.sonCalisilanSinav = sinav.sinav_ad;
+                        bilgiler.sinavTarihi = sinav.sinav_tarih.ToShortDateString();
+                        bilgiler.derseSonCalisilanZaman = calismalar.LastOrDefault().calisilan_zaman.ToString();
+
+                        //Sınava Çalışılan Toplam Zaman
+                        TimeSpan sinavToplam = TimeSpan.Zero;
+                        foreach (var calisma in calismalar)
+                        {
+                            if (calisma.sinav_id == sinav.id)
+                                sinavToplam += calisma.calisilan_zaman;
+                        }
+                        bilgiler.sinavaCalisilanZaman = sinavToplam.ToString();
+                    }
+
+                    //Son Ders
+                    int? sonDersId = calismalar.LastOrDefault().ders_id;
+                    if (sonDersId == null)
+                        bilgiler.sonCalisilanDers = "-";
+                    else
+                    {
+                        var ders = _dersService.GetById((int)sonDersId);
+                        bilgiler.sonCalisilanDers = ders.ders_ad;
+                        bilgiler.sonCalismaTarihi = calismalar.LastOrDefault().calisilan_tarih.ToString();
+
+                        //Derse Çalışılan Toplam Zaman
+                        TimeSpan dersToplam = TimeSpan.Zero;
+                        foreach (var calisma in calismalar)
+                        {
+                            if (calisma.ders_id == sonDersId)
+                                dersToplam += calisma.calisilan_zaman;
+                        }
+                        bilgiler.derseCalisilanZaman = dersToplam.ToString();
+                    }
+                }
+                else
+                {
+                    bilgiler.sinavaCalisilanZaman = "-";
+                    bilgiler.derseCalisilanZaman = "-";
                     bilgiler.sonCalisilanDers = "-";
-                else
-                {
-                    var ders = _dersService.GetById((int)sonDersId);
-                    bilgiler.sonCalisilanDers = ders.ders_ad;
-                    bilgiler.sonCalismaTarihi = calismalar.LastOrDefault().calisilan_tarih.ToString();
-
-                    //Derse Çalışılan Toplam Zaman
-                    TimeSpan dersToplam = TimeSpan.Zero;
+                    bilgiler.sonCalisilanSinav = "-";
+                    bilgiler.sonCalismaTarihi = "-";
+                    bilgiler.toplamCalisilanZaman = "-";
+                    bilgiler.sinavTarihi = "-";
+                    bilgiler.derseSonCalisilanZaman = "-";
+                }
+                return bilgiler;
+            }
+            else if (durum == 1) //Çalışma Bilgileri
+            {
+                if (calismalar.Count > 0)
+                {                    
+                    TimeSpan toplam = TimeSpan.Zero;
                     foreach (var calisma in calismalar)
                     {
-                        if (calisma.ders_id == sonDersId)
-                            dersToplam += calisma.calisilan_zaman;
+                        if (calisma.sinav_id == sinav_id)
+                            toplam += calisma.calisilan_zaman;
                     }
-                    bilgiler.derseCalisilanZaman = dersToplam.ToString();
+                    bilgiler.sinavaCalisilanZaman = toplam.ToString();
+
+                    var sinav = _sinavService.GetById((int)sinav_id);
+                    int kalan = Convert.ToInt32((sinav.sinav_tarih - DateTime.Now).TotalDays);
+                    if (kalan < 0)
+                        bilgiler.sinavaKalanGun = "Sınav süresi geçti";
+                    else
+                        bilgiler.sinavaKalanGun = kalan.ToString();
+
+                    bilgiler.data = 1;
                 }
+                else
+                {
+                    bilgiler.data = 0;
+                    bilgiler.sinavaCalisilanZaman = "-";
+                    bilgiler.sinavaKalanGun = "-";                    
+                }
+                return bilgiler;   
             }
-            else
+            else //Ders Bilgileri
             {
-                bilgiler.sinavaCalisilanZaman = "-";
-                bilgiler.derseCalisilanZaman = "-";
-                bilgiler.sonCalisilanDers = "-";
-                bilgiler.sonCalisilanSinav = "-";
-                bilgiler.sonCalismaTarihi = "-";
-                bilgiler.toplamCalisilanZaman = "-";
-                bilgiler.sinavTarihi = "-";
-                bilgiler.derseSonCalisilanZaman = "-";
-            }           
-            return bilgiler;
+                if(calismalar.Count > 0)
+                {
+                    TimeSpan toplam = TimeSpan.Zero;
+                    foreach (var calisma in calismalar)
+                    {
+                        if (calisma.ders_id == ders_id)
+                            toplam += calisma.calisilan_zaman;
+                    }
+                    bilgiler.derseCalisilanZaman = toplam.ToString();
+
+                    bilgiler.data = 1;
+                }
+                else
+                {
+                    bilgiler.data = 0;
+                    bilgiler.derseCalisilanZaman = "-";
+                }
+                return bilgiler;
+            }
         }        
         
         [HttpGet]
-        public JsonResult GetDers(int sinavId)
+        public JsonResult GetDers(int sinavId) //Ders List için dersler
         {
             if (sinavId > 0)
             {
@@ -118,8 +171,45 @@ namespace StudyCheckWeb.MvcWebUI.Areas.Study.Controllers
                     return Json(new { success = true, value = "Veri Yok" });
             }
             else
-                return Json(new {success=false,value = "Başarısız" });
-            
+                return Json(new {success=false,value = "Başarısız" });            
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetSinavBilgileri(int sinavId)
+        {
+            if (sinavId > 0)
+            {
+                var identityUser = await _userManager.GetUserAsync(HttpContext.User);
+                List<Calisma> calismalar = _calismaService.GetListByUyeId(identityUser.uyeDetayId);
+                CalismaBilgileri bilgiler = studyInfos(calismalar, 1, sinavId);
+                string sinavAd = _sinavService.GetById(sinavId).sinav_ad;
+                if(bilgiler.data == 1)
+                    return Json(new { success = true, topTime = bilgiler.sinavaCalisilanZaman, kalanGun = bilgiler.sinavaKalanGun,sinavAdi = sinavAd });
+                else
+                    return Json(new { success = true, topTime = "-", kalanGun = "-" , sinavAdi = "-"});
+            }
+            else
+                return Json(new { success = false, value = "Başarısız" });
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetDersBilgileri(int dersId)
+        {
+            if(dersId > 0)
+            {
+                var identityUser = await _userManager.GetUserAsync(HttpContext.User);
+                List<Calisma> calismalar = _calismaService.GetListByUyeId(identityUser.uyeDetayId);
+                CalismaBilgileri bilgiler = studyInfos(calismalar, 2,null,dersId);
+                string dersAd = _dersService.GetById(dersId).ders_ad;
+                if (bilgiler.data == 1)
+                    return Json(new { success = true, dtopTime = bilgiler.derseCalisilanZaman, dersAdi = dersAd });
+                else
+                    return Json(new { success = true, dtopTime = "-", dersAdi = "-" });
+            }
+            else
+            {
+                return Json(new { success = false, value = "Başarısız" });
+            }
         }
 
         public async Task<IActionResult> Index()
